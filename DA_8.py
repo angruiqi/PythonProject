1952454d96036bc215168b23739a780a0c92fa06
angruiqi <angruiqi21@gmail.com>
2021-08-26 17:44:53 +0800

    Initial commit

---
+++ DA_8.py
@@ -0,0 +1,68 @@
+import requests
+import scrapy
+import unittest
+
+
+# requesting for a get request from https://www.nike.com/sg/
+url = "https://brickset.com/sets/year-2005"
+user_agent = {'User-Agent': 'Mobile'}
+r = requests.get(url)
+
+print("Status code:")
+print("\t *", r.status_code)
+
+# Requesting for web header
+h = requests.head(url)
+print("Web Header:")
+print("**********")
+
+# To print out the Web Header line by line
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("**********")
+
+# Requesting for CLient header then printing it out
+response = requests.get(url, headers=user_agent)
+print("User-Agent:,")
+print("**********")
+print(response.request.headers)
+print("**********")
+
+class NewSpider(scrapy.Spider):
+    name = "new_spider"
+    start_urls = ['https://brickset.com/sets/year-2005']
+
+    def parse(self, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel = '@src'
+            yield {
+                    'Image Link': x.xpath(newsel).extract_first(),
+            }
+
+
+        Page_selector = '.next a ::attr(href)'
+        next_page = response.css(Page_selector).extract_first()
+        if next_page:
+                yield scrapy.Request(
+                        response.urljoin(next_page),
+                        callback=self.parse
+        )
+
+
+class TestMyProgram(unittest.TestCase):
+    url2 = 'https://brickset.com/sets/year-2005'
+    headers = {'User-Agent': 'Mobile'}
+    rh = requests.get(url2, headers=headers)
+    statuscode = rh.status_code
+    user_agent = rh.request.headers
+
+    def test_statuscode(self):
+        self.assertEqual(TestMyProgram.statuscode, 200)
+
+    def test_headers(self):
+        self.assertTrue(TestMyProgram.user_agent, "Mobile")
+
+
+if __name__ == '__main__':
+    unittest.main()
